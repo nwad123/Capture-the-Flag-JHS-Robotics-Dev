a4a2e93e21a05d4ef462ff3df836cb6a260e222d
nwad123 <33466551+nwad123@users.noreply.github.com>
2017-11-09 13:24:48 -0700

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -3,41 +3,26 @@ package org.usfirst.frc.team6411.robot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the RobotDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled by the switches on the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
+
 public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 1);
-	Joystick controller1 = new Joystick(0);
-	Joystick controller2 = new Joystick(1);
+	RobotDrive DriveTrain = new RobotDrive(0, 1, 2, 3);
+	Joystick controller = new Joystick(0);
 	final String defaultAuto = "Default";
 	final String customAuto = "My Auto";
 	SendableChooser<String> chooser = new SendableChooser<>();
-	Compressor c = new Compressor(0);
-	boolean compressorBoolean = false;
 	
-
+	/*****Pneumatics*************************************************************/
+	Compressor airBooster = new Compressor(0);
+	DoubleSolenoid magicAir = new DoubleSolenoid(2,3);
+	
 	public Robot() {
-		myRobot.setExpiration(0.1);
+		DriveTrain.setExpiration(0.1);
 	}
 
 	@Override
@@ -45,89 +30,53 @@ public class Robot extends SampleRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto modes", chooser);
-	
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomous() {
-		String autoSelected = chooser.getSelected();
-		// String autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-
-		switch (autoSelected) {
-		case customAuto:
-			myRobot.setSafetyEnabled(false);
-			myRobot.drive(-0.5, 1.0); // spin at half speed
-			Timer.delay(2.0); // for 2 seconds
-			myRobot.drive(0.0, 0.0); // stop robot
-			break;
-		case defaultAuto:
-		default:
-			myRobot.setSafetyEnabled(false);
-			myRobot.drive(-0.5, 0.0); // drive forwards half speed
-			Timer.delay(2.0); // for 2 seconds
-			myRobot.drive(0.0, 0.0); // stop robot
-			break;
-		}
-	}
-
-	/**
-	 * Runs the motors with arcade steering.
-	 */
-	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(controller1, controller2); // drive with arcade style (use right
-										// stick)
-			Timer.delay(0.005); // wait for a motor update time
-			
-			compressorBoolean = controller1.getRawButton(1);
-			
-			if(compressorBoolean == true)
-			{
-				c.setClosedLoopControl(true);
-			}
-			else
-			{
-				c.setClosedLoopControl(false);	
-			}
 		
-			
-			
-			
-			boolean pressureSwitch = c.getPressureSwitchValue();
-			if(pressureSwitch == false)
-			{
-				c.setClosedLoopControl(false);
-			}
-			
-			
-			
-			
-			
-//		THESE ARE USED TO MAKE THE PRESSURE SWITCH WORK	
-//			boolean enabled = c.enabled();
-//			boolean current = c.getClosedLoopControl();
-		}
+		airBooster.setClosedLoopControl(true);
 	}
 
-	/**
-	 * Runs during test mode
-	 */
-	@Override
-	public void test() {
-	}
+	public void disabledInit(){
+
+    }
+
+    public void disabledPeriodic() {
+      
+    }
+
+    public void autonomousInit() {
+       
+        
+    }
+
+    public void autonomousPeriodic() {
+        
+    }
+
+    public void teleopInit() {
+       
+    }
+
+    double vmotion = 0;
+    double tmotion = 0;
+    
+    public void teleopPeriodic() {
+       
+    	vmotion = controller.getY();
+    	tmotion = controller.getX();
+    	
+    	DriveTrain.arcadeDrive(controller);
+    	
+    	airBooster.setClosedLoopControl(true);
+    	
+    	if(controller.getRawButton(1) == true) {
+    		magicAir.set(DoubleSolenoid.Value.kForward);
+    	}
+    	else {
+    		magicAir.set(DoubleSolenoid.Value.kReverse);
+    	}	
+    	
+    }
+
+    public void testPeriodic() {
+        
+    }
 }
